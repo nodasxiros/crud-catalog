db2e0eb4ded4923f5b493d4b7318a3f6be4c0f7e
Nodas <nodasxiros@gmail.com>
2022-07-08 10:10:02 +0300

    Added sequelize orm module and updated gitignore and env.example files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { BeersModule } from './beers/beers.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      models: [],
+    }),
+    BeersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
