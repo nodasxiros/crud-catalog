a8e0a8ef5ef342f3b293c8976536e92de2e46205
Nodas <nodasxiros@gmail.com>
2022-07-08 22:48:26 +0300

    Added AppRouter component to render function

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppRouter from './router/AppRouter';
+import './index.scss'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AppRouter />
 );
 
 // If you want to start measuring performance in your app, pass a function
